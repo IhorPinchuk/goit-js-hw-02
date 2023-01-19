304e400c7a173ee167220d0f7150b05912a55b8f
IhorPinchuk <pinchukkk@ukr.net>
2023-01-19 16:54:46 +0200

    task-26

--- js/script-1.js
+++ js/script-1.js
@@ -248,16 +248,182 @@
 
 // ------------------------------task 18------------------------------
 
-function calculateTotal(number) {
- // Change code below this line
-    
+// function calculateTotal(number) {
+//   // Change code below this line
+//   let totalNumbers = 0;
+//   for (let i = 1; i <= number; i += 1) {
+//     totalNumbers += i;
+//   }
 
-  // Change code above this line
-}
+//   return totalNumbers;
+//   // Change code above this line
+// }
 
-console.log(calculateTotal(1));
-console.log(calculateTotal(3));
-console.log(calculateTotal(7));
-console.log(calculateTotal(18));
-console.log(calculateTotal(24));
-console.log(calculateTotal());
+// console.log(calculateTotal(1));
+// console.log(calculateTotal(3));
+// console.log(calculateTotal(7));
+// console.log(calculateTotal(18));
+// console.log(calculateTotal(24));
+// console.log(calculateTotal());
+
+// ------------------------------task 19------------------------------
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) { // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
+// ------------------------------task 20------------------------------
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+//   for (let element of order) {
+//     total += element;
+// }
+//   // Change code above this line
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// ------------------------------task 21------------------------------
+
+// function findLongestWord(string) {
+//   // Change code below this line
+//   const words = string.split(' ');
+//   let longestWordString = words[0];
+
+//   for (let word of words) {
+//     if (word.length > longestWordString.length) {
+//       longestWordString = word;
+//     }
+//   }
+
+//   return longestWordString;
+//   // Change code above this line
+// }
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
+
+// ------------------------------task 22------------------------------
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i)
+//   }
+//   // Change code above this line
+//   return numbers;
+// }
+
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
+
+// ------------------------------task 23------------------------------
+
+// ------------------------------Варіант 1------------------------------
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//   let moreValue = [];
+//   for (let i = 0; i <= numbers.length; i += 1) {
+//     console.log(i);
+//     if (numbers[i] > value) {
+//       moreValue.push(numbers[i]);
+// }
+//   }
+//   return moreValue;
+
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// ------------------------------Варіант 2------------------------------
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//   let moreValue = [];
+//   for (const number of numbers) {
+//      if (number > value) {
+//       moreValue.push(number);
+// }
+//   }
+//   return moreValue;
+
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// ------------------------------task 24------------------------------
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit); // Change this line
+// }
+
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+// ------------------------------task 25------------------------------
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   let commonArrayNumbers = [];
+//   for (const number of array1) {
+//     if (array2.includes(number)) {
+//       commonArrayNumbers.push(number);
+//    }
+//   }
+  
+//   return commonArrayNumbers;
+//  // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+// ------------------------------task 26------------------------------
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (const element of order) {
+//     total += element;
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([]));
+
+// ------------------------------task 27------------------------------
